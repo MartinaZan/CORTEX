adcb10ebe62b57ad6ed78b03a7006e8652df8576
MartinaZan <martina.zannotti@unicam.com>
2024-11-27 15:14:09 +0100

    Modifiche 27 novembre

--- src/dataset/generators/gcs.py
+++ src/dataset/generators/gcs.py
@@ -2,8 +2,6 @@ from os.path import join
 
 import json
 import numpy as np
-import pandas as pd
-
 import random
 
 from src.dataset.generators.base import Generator
@@ -29,37 +27,45 @@ class GCS(Generator):
         if 'data_label_name' not in local_config['parameters']:
             raise Exception("The name of the label column must be given.")
 
+    # Funzione per la generazione del dataset, che chiama la funzione per la lettura dei dati dal json
     def generate_dataset(self):
         if not len(self.dataset.instances):
             self._read(self._data_file_path)
 
     def _read(self, path):
+        # Apertura del file json con dati dei grafi.
+        # Il dizionario comprende le seguenti chiavi:
+        #    data.keys() --> dict_keys(['edge_mapping', 'node_ids', 'time_periods', 'target', 'series'])
+        # 'target' contiene le true label per la classificazione
+        # 'series' contiene i valori della serie temporale multivariata, da usare per definire le feature dei nodi
         data = json.load(open(path))
-        ### data.keys() --> dict_keys(['edge_mapping', 'node_ids', 'time_periods', 'target', 'series'])
-        ###   In 'target' ho la label della classificazione
-        ###   In 'series' ho i valori della serie (da mettere come feature dei nodi)
 
+        # Definizione di node e edge geatures map. Sono dei dizionari che contengono tante caratteristiche quante sono le feature.
         self.dataset.node_features_map = {'attribute_0': 0}
         # self.dataset.edge_features_map =  # change me
 
+        # Creazione di un grafo per ogni step temporale
         for t in data['time_periods']:
-            # OLD (da eliminare)
-            #A,X,W = corr2graph(t, data['edge_mapping']['edge_index'][str(t)], data['edge_mapping']['edge_weight'][str(t)], data["series"][t-49], self.dataset)
-            #y = data["target"][t-49]
-            #if (t-49) % 3 == 0:
-            #    y = 1
-
-            # NEW
+            # Creazione di adjacency e node feature matrix.
+            # Alla funzione corr2graph vengono passati:
+            #   - l'id del grafo (ossia l'indice temporale): t
+            #   - le informazioni sugli archi al tempo t: data['edge_mapping']['edge_index'][str(t)]
+            #   - le informazioni sui pesi al tempo t: data['edge_mapping']['edge_weight'][str(t)]
+            #   - i valori della serie temporale al tempo t: data["series"][t]
+            #   - self.dataset (per accedere a self.dataset.node_features_map e self.dataset.edge_features_map)
+            # Nota: per poter usare il primo json creato, occorre mettere t-49 in data["series"][t-49] e y = data["target"][t-49]
             A,X,W = corr2graph(t, data['edge_mapping']['edge_index'][str(t)], data['edge_mapping']['edge_weight'][str(t)], data["series"][t], self.dataset)
+            
+            # Lettura della true label al tempo t (crisi/no crisi)
             y = data["target"][t]
 
             g = GraphInstance(
-                id = t,
-                label = y,          # crisi/no crisi
-                data = A,           # data: n × n matrix where n is the number of nodes (i.e., it is the binary adjacency matrix)
-                node_features = X,  # node_features: n × d matrix where d is the number of node features (per ora considero d = 1)
-                edge_weights = W,   # edge_weights: n × n matrix containing the weight of each edge (i.e., it is the weighted adjacency matrix)
-                graph_features = {}
+                id = t,                 # id temporale
+                label = y,              # label crisi/no crisi
+                data = A,               # data: n x n matrix where n is the number of nodes (i.e., it is the binary adjacency matrix)
+                node_features = X,      # node_features: n x d matrix where d is the number of node features (per ora considero d = 1)
+                edge_weights = W,       # edge_weights: n x n matrix containing the weight of each edge (i.e., it is the weighted adjacency matrix)
+                graph_features = {}     # feature del grafo (per il momento, nessuna)
             )
             self.dataset.instances.append(g)
     
@@ -67,18 +73,21 @@ def corr2graph(id, data, weight, series, dataset):
     # n_map = dataset.node_features_map
     # e_map = dataset.edge_features_map
     
+    # Numero di nodi
     n = 24
 
     A = np.zeros((n, n))
     W = np.zeros((n, n))
 
-    X = np.array(series).reshape(24, 1)
+    X = np.array(series).reshape(n, 1)
 
+    # Scorre tutti gli archi e imposta matrice di adiacenza pesata (W) e binaria (A)
     for p, edge in enumerate(data):
-        #if np.abs(weight[p]) > 0.3:
+        #if np.abs(weight[p]) > 0.3: # (eventuale condizione per impostare un threshold)
         W[edge[0], edge[1]] = np.abs(weight[p])
         A[edge[0], edge[1]] = 1 if weight[p] != 0 else 0
 
+    # W deve essere un vettore, quindi lo flatteniamo
     W = W[W != 0].flatten()
 
     return A,X,W
