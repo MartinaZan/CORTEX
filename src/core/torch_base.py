bb84861683f8bece5d020f9e9985bff9e137f0a3
MartinaZan <martina.zannotti@unicam.com>
2024-12-12 16:10:18 +0100

    Modifiche notebook esperimento

--- src/core/torch_base.py
+++ src/core/torch_base.py
@@ -13,7 +13,7 @@ from torch_geometric.loader import DataLoader
 class TorchBase(Trainable):
        
     def init(self):
-        self.epochs = 15 # self.local_config['parameters']['epochs']
+        self.epochs = 20 # self.local_config['parameters']['epochs']
         self.batch_size = self.local_config['parameters']['batch_size']
         
         self.model = get_instance_kvargs(self.local_config['parameters']['model']['class'],
